3be77aed4c5d6c111aeb3566cc83519934079c1e
Himel-Nath <himelnath6807@gmail.com>
2025-07-06 18:06:56 -0300

    Projects section

---
+++ src/components/PageContent/FifthPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <h1 class="hidden-text title">His Creations - Cont.</h1>
+      <h3 class="hidden-text sub-title">Discord Bot (Dated January 2023 - April 2023)</h3>
+      <figure class="footage">
+        <video class="discord" muted playsinline autoplay loop src="/discord.mp4"></video>
+        <figcaption class="caption">Footage captured some time in 2023.</figcaption>
+      </figure>
+
+      <p class="hidden-text page-text">
+        This bot appeared without fanfare but it quickly made noise. Its users could track real-time
+        game discounts, manage wishlists, check hours played, leave reviews, and get live updates on
+        the latest games releasing. The idea was simple: give gamers more control over their time,
+        their spending, their backlog. Footage of the bot in action began circulating, submitted by
+        loyal users who relied on it for months. Forensic scientists later uncovered the framework
+        behind it: Python, working in tandem with AWS Lambda, Dynamo DB, EventBridge, S3, and EC2.
+      </p>
+      <br />
+      <h3 class="hidden-text sub-title">The Daily Portfolio (May 2025 - )</h3>
+      <p class="hidden-text page-text">
+        Even after entering the workforce (see page 6), he didn't stop. Few have seen it in full.
+        Those who have say it was built with Vue and handcrafted with care. A site like this,
+        immersive, responsive, and full of subtle interactivity, could be dangerous in the wrong
+        hands. Our editors were unable to secure live footage, but witnesses speak of glowing
+        cursors, moving images, and a layout that doesn't behave like normal websites.
+      </p>
+    </div>
+    <span class="page-number">6</span>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+</script>
+
+<style lang="css">
+.discord {
+  height: 225px;
+  filter: grayscale(75%);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.6);
+  border: 2px solid #222;
+  margin-right: 1rem;
+}
+
+.footage {
+  float: right;
+}
+</style>
--- src/components/PageContent/FourthPage.vue
+++ src/components/PageContent/FourthPage.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="parchment-wrapper">
     <div class="book-theme">
-      <h1 class="hidden-text">Page 4</h1>
-      <p class="hidden-text">This is the content of the fourth page.</p>
+      <h1 class="hidden-text title">His Creations</h1>
+      <h3 class="hidden-text sub-title">Trello Clone (Dated May 2022 - August 2022)</h3>
+      <figure>
+        <video class="trello" muted playsinline autoplay loop src="/trello.mp4"></video>
+        <figcaption class="caption">Final protoype shown in spring 2024.</figcaption>
+      </figure>
+      <p class="hidden-text page-text">
+        The first signs appeared early. Working with a team of five, he developed a full-stack task
+        management system to coordinate group efforts. Users could register profiles, create
+        multi-list workflows, assign tasks, and track progress in real time. Later, outside the
+        confines of academia, he returned to it. Alone. Written in Java (Spring Boot) and React, he
+        was able to strip out bloat, modularize components, and introduce subtle but impactful
+        improvements.
+      </p>
+      <br />
+      <h3 class="hidden-text sub-title">Spotify Automation (Dated - Unknown)</h3>
+      <p class="hidden-text page-text">
+        Not every build was large. Some were built with purpose. This one began as a personal tool,
+        a Python script designed to generate Spotify playlists from a single text file. Without any
+        UI or setup, it eliminated the need for manual playlist creation and streamlined a
+        repetitive task into a single step. It let users spend less time managing music, and more
+        time actually listening to it.
+      </p>
     </div>
+    <span class="page-number">5</span>
   </div>
 </template>
 
 <script setup>
 import '@/assets/book-theme.css'
 </script>
+
+<style lang="css">
+.trello {
+  height: 250px;
+  filter: grayscale(75%);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.6);
+  border: 2px solid #222;
+}
+</style>
