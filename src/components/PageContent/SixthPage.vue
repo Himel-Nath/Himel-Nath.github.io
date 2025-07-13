b69a625a51ff0b94bee9e8c5be5282351a77d707
Himel-Nath <himelnath6807@gmail.com>
2025-07-13 17:50:35 -0300

    page six

---
+++ src/components/PageContent/SixthPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <h1 class="hidden-text title">Where he's worked</h1>
+      <span class="page-number">7</span>
+      <p class="hidden-text page-text">
+        His first and current full time position is covered in page 9, while we explore the roles in
+        between here. We turned to coworkers, managers, and those who saw him in the day-to-day.
+        What emerged wasn't a straight line, but a pattern of curiousity, adaptability, and control.
+      </p>
+      <br />
+      <h3 class="hidden-text sub-title">SolutionInc (Dated January 2022 - April 2022)</h3>
+      <p class="hidden-text page-text">
+        Configuring mini-PCs, gathering client requirements, and supporting connectivity for several
+        users, he adopted a technical shift. He wasn't there to write code, he was there to observe;
+        how systems failed, how people responded, and how to fill in the gaps. One manager recalled
+        how he volunteered to join a site survey, an unusual task for an intern, just to understand
+        network design and the installation process.
+      </p>
+
+      <blockquote class="page-text hidden-text quote">
+        “Outstanding ability to work independently on assigned work and initiates new tasks.”
+        <footer>- Manager, SolutionInc</footer>
+      </blockquote>
+
+      <h3 class="hidden-text sub-title">Dash Hudson (Dated September 2022 - December 2022)</h3>
+      <p class="hidden-text page-text">
+        They say he built pages from scratch using Vue, composing interactive interfaces with
+        meticulous precision. Every part was built for purpose. He didn't stop at the surface.
+        Beneath it, he constructed Flask endpoints to support these features, each with its own set
+        of test cases. One such feature was Google Analytics, which was woven right into the
+        platform to track behaviorial data pulled from social media. Colleagues mentioned he was
+        deeply involved within the agile cycle, contributing to story point estimates, code reviews,
+        and running daily scrum meetings.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+</script>
+
+<style lang="css"></style>
