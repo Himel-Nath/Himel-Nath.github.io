9685ee15f0ff64e00db4ee7f8314d932bbe10f82
Himel-Nath <himelnath6807@gmail.com>
2025-07-19 19:31:38 -0300

    full time section

---
+++ src/components/PageContent/EightPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <h1 class="hidden-text title">The Industry</h1>
+      <span class="page-number">9</span>
+      <p class="hidden-text page-text">
+        Sources say SimplyCast was seeking for a brilliant mind still early in development. They
+        found him. Within weeks, he was already speaking the team's lingo, diagnosing bugs,
+        navigating the codebase and improving the platform. His supervisor [name redacted] reported
+        that he completed 34 tasks, more than anyone else that term, often asking for additional
+        work. We were able to scavenge these remarks from the Co-Op appraisal report:
+      </p>
+
+      <blockquote class="page-text hidden-text quote">
+        “Himel is a highly productive worker and exceeded all my expectations for quality and
+        quantity of work from a student on his co-op term. He shows great problem-solving skills, a
+        keen desire to learn, and has a great work ethic. He would do well in pretty much any web
+        development job.”
+        <footer>- Supervisor, SimplyCast</footer>
+      </blockquote>
+
+      <p class="hidden-text page-text">
+        They weren't wrong. It was never supposed to be permanent. But when he left, something had
+        already been set in motion. They had plans for him and he had unfinished business.
+      </p>
+      <p class="hidden-text page-text">
+        He returned to SimplyCast after his graduation, but this time not as an intern. He picked up
+        where he left off, fixing bugs, big and small. But the stakes had changed. He tackled
+        customer-impacting and time-sensitive issues. Within weeks, critical issues became routine.
+        Two months in, he was promoted to a supervisor role. He overlooked two teams - 4 students,
+        across 8 months. He ran daily scrums, delegated tasks, guided investigations, and reviewed
+        submissions.
+      </p>
+
+      <blockquote class="page-text hidden-text quote">
+        “I felt supported throughout my work term… he always checked up on me.”
+        <footer>- Student, Fall 2024 Co-Op Term</footer>
+      </blockquote>
+      <blockquote class="page-text hidden-text quote">
+        “He was always available to answer my questions and help me out whenever.”
+        <footer>- Student, Winter 2025 Co-Op Term</footer>
+      </blockquote>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+</script>
+
+<style lang="css"></style>
---
+++ src/components/PageContent/FullTime.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <h1 class="hidden-text title">The Industry - Cont.</h1>
+      <span class="page-number">10</span>
+
+      <p class="hidden-text page-text full-time-text">
+        <video
+          src="/canva-ad2.mp4"
+          alt="canva"
+          class="canva-ad2"
+          muted
+          playsinline
+          autoplay
+          loop
+        ></video>
+        As he settled into the role, he became a link between departments; coordinating deadlines
+        with sales, aligning expectations with QA, and directing DevOps through critical
+        deployments. Over three months, the bug count dropped from 220 to 99. The CEO took notice,
+        publicly recognizing his impact during the company meeting.<br /><br />
+        Still not satisfied, he documented a streamlined onboarding process, reducing friction for
+        every new developer. Unprompted, he also wrote a custom script to automate task
+        unassignment, a tool adopted and praised by other managers for its efficiency. He was pulled
+        into a migration and upgrade project, testing apps, identifying deprecated code, and
+        researching safe transitions. Not long after, he was promoted to Feature Developer. Now he
+        builds full-stack features. Angular for interfaces, PHP and MySQL on the backend. Designing
+        APIs, building dashboards, writing client-requested functionality from scratch. Three roles,
+        one year.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+</script>
+
+<style lang="css">
+.canva-ad2 {
+  float: left;
+  height: 350px;
+  margin: 0.5rem;
+}
+</style>
