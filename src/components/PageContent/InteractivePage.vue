e17c574af5e8b70756b9162c4d6832dbc4ff4585
Himel-Nath <himelnath6807@gmail.com>
2025-07-21 21:58:30 -0300

    Interactive page

---
+++ src/components/PageContent/InteractivePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <span class="page-number">13</span>
+      <img src="/abstract.png" alt="abstract art" class="abstract-art" />
+      <h1 class="hidden-text title">The Puzzle Corner</h1>
+      <WordSearch />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+import WordSearch from '../WordSearch.vue'
+</script>
+
+<style lang="css">
+.abstract-art {
+  height: 16rem;
+  margin-top: 4rem;
+}
+</style>
