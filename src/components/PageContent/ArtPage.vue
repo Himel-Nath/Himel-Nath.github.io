de799bfee6826ced602bee05d5844b4b1e6f30b6
Himel-Nath <himelnath6807@gmail.com>
2025-07-21 21:48:28 -0300

    Art page

---
+++ src/components/PageContent/ArtPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="parchment-wrapper">
+    <div class="book-theme">
+      <h1 class="hidden-text title">The Gallery</h1>
+      <span class="page-number">12</span>
+      <img src="/sketches.png" alt="portfolio sketches" class="sketches" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import '@/assets/book-theme.css'
+</script>
+
+<style lang="css">
+.sketches {
+  height: 42rem;
+}
+</style>
