97bf77bf891f6a0fdc3ddf462c8e858f96dbdf14
Himel-Nath <himelnath6807@gmail.com>
2025-07-28 22:38:21 -0300

    fixes

--- src/components/PageContent/IntroPage.vue
+++ src/components/PageContent/IntroPage.vue
@@ -3,17 +3,17 @@
     <div class="book-theme">
       <h1 class="hidden-text title">What we know</h1>
       <p class="hidden-text page-text intro-text">
-        His name is <strong>Himel Dev Nath</strong>. Born in Bangladesh, his last confirmed sighting
-        places him at age 24 in <strong>Halifax, Nova Scotia, Canada</strong>. While the details
-        remain sparse, being informed about his background may help infer his future whereabouts.
-        According to his family, his relocation to Canada was driven by ambition, a desire to pursue
-        higher education and get closer to his larger goal. His Professors confirm that he not only
-        adapted, but excelled, using his growing knowledge to devise projects with unclear purposes
-        (more on his projects in page 5). What complicates his profile is his unpredictability.
-        Those closest to him cite strong interests in fine arts, literature and video games. While
-        seemingly unrelated, these hobbies hint at a mind that refuses to sit still, creatively and
-        geographically. His movements span the Middle East, Asia, Europe, and North America. It's
-        anyone's guess where he's headed next.
+        His name is <strong>Himel Dev Nath</strong>. Born in Bangladesh and raised in Oman, his last
+        confirmed sighting places him at age 24 in <strong>Halifax, Nova Scotia, Canada</strong>.
+        While the details remain sparse, being informed about his background may help infer his
+        future whereabouts. According to his family, his relocation to Canada was driven by
+        ambition, a desire to pursue higher education and get closer to his larger goal. His
+        Professors confirm that he not only adapted, but excelled, using his growing knowledge to
+        devise projects with unclear purposes (more on his projects in page 5). What complicates his
+        profile is his unpredictability. Those closest to him cite strong interests in fine arts,
+        literature and video games. While seemingly unrelated, these hobbies hint at a mind that
+        refuses to sit still, creatively and geographically. His movements span the Middle East,
+        Asia, Europe, and North America. It's anyone's guess where he's headed next.
       </p>
       <img src="/world-map.gif" loading="lazy" alt="world map" class="map" />
     </div>
--- src/components/PageContent/ThirdPage.vue
+++ src/components/PageContent/ThirdPage.vue
@@ -10,18 +10,17 @@
       </figure>
       <p class="hidden-text page-text third-text">
         His Professors and Dean took notice of his performance, awarding him the
-        <strong
-          >Sexton Scholar designation multiple times for maintaining a GPA above 3.85. Shown here is
-          a certificate recognizing him for the highest CGPA in his cohort</strong
-        >. Final year allowed him to explore different realms by choosing Network Security, Cloud
-        Computing and Cryptography as his electives. He graduated with a
-        <strong>certificate in Cybersecurity</strong> to complement his degree. But that wasn't all.
-        When others chose easy electives, he pursued a <strong>minor in Mathematics</strong>.
-        Courses like Advanced Linear Algebra, Ordinary Differential Equations, and Multivariable
-        Calculus fed a passion he never let go of. His academic record closed with a
-        <strong>CGPA of 4.16</strong> despite not having written a single line of code before
-        University. If that doesn't make him dangerous, it makes him unpredicatable. And in this
-        case, that might be worse.
+        <strong>Sexton Scholar</strong> designation multiple times for maintaining a
+        <strong>GPA above 3.85.</strong> Shown here is a certificate recognizing him for the
+        <strong>highest CGPA</strong> in his cohort. Final year allowed him to explore different
+        realms by choosing Network Security, Cloud Computing and Cryptography as his electives. He
+        graduated with a <strong>certificate in Cybersecurity</strong> to complement his degree. But
+        that wasn't all. When others chose easy electives, he pursued a
+        <strong>minor in Mathematics</strong>. Courses like Advanced Linear Algebra, Ordinary
+        Differential Equations, and Multivariable Calculus fed a passion he never let go of. His
+        academic record closed with a <strong>CGPA of 4.16</strong> despite not having written a
+        single line of code before University. If that doesn't make him dangerous, it makes him
+        unpredicatable. And in this case, that might be worse.
       </p>
     </div>
     <span class="page-number">4</span>
